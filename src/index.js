46dfa7e5c87006d0addc938e2baed7a3af32888a
Srinjani Roy Chowdhury <roysrinjani04@gmail.com>
2026-03-16 00:01:21 +0530

    add api utillity classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { app } from "./app.js";
 import { configDotenv } from "dotenv";
 import connectDB from "./db/index.js";
 configDotenv({
@@ -5,3 +6,16 @@ configDotenv({
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on PORT ${process.env.PORT}`);
+    });
+
+    app.on("error", (error) => {
+        console.log(`ERROR ${error}`);
+        throw error;
+    });
+})
+.catch((err) => {
+    console.log(`MONGO db connection failed!! ${err}`);
+})
