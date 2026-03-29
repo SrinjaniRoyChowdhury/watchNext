e4cb7f88c881caaca5260805ef8e466fce14a072
Srinjani Roy Chowdhury <roysrinjani04@gmail.com>
2026-03-29 22:42:52 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from express;
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -13,5 +13,12 @@ app.use(express.json({limit: "10kb"}))
 app.use(express.urlencoded({extended: true, limit: "10kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
 
 export {app};
